df289f446e65cca50cdbce3276d0a1567278d99b
Paul Sorensen <p-sorensen@onu.edu>
2015-09-11 04:24:14 +0000

    added analytics

--- parser.rb
+++ parser.rb
@@ -932,7 +932,17 @@ while true
               <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
               <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
               <title>A Better Student L Digest</title>
-            </head>
+            <script>
+  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+
+  ga("create", "UA-67474823-1", "auto");
+  ga("send", "pageview");
+
+</script>
+</head>
                 <nav class="top-nav orange flow-text">
                   <div class="container">
                     <div class="nav-wrapper"><a class="page-title black-text"><strong>A Better Student L Digest</strong></a></div>
