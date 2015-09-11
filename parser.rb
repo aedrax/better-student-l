6141bf9f50db247651f71c857e1066b84a1955e4
Paul Sorensen <p-sorensen@onu.edu>
2015-09-11 15:36:46 -0400

    fixed formatting

--- parser.rb
+++ parser.rb
@@ -100,7 +100,8 @@ while true
             
         }
         
-        html_header = '<!DOCTYPE html>
+        html_header = 
+	'<!DOCTYPE html>
           <html>
             <head>
               <!--Import materialize.css-->
@@ -111,22 +112,25 @@ while true
               <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
               <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
               <title>A Better Student L Digest</title>
-            <script>
-  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,"script","//www.google-analytics.com/analytics.js","ga");
+              <script>
+		(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,"script","//www.google-analytics.com/analytics.js","ga");
 
-  ga("create", "UA-67474823-1", "auto");
-  ga("send", "pageview");
-
-</script>
-</head>
-                <nav class="top-nav orange flow-text">
-                  <div class="container">
-                    <div class="nav-wrapper"><a class="page-title black-text"><strong>A Better Student L Digest</strong></a></div>
-                  </div>
-                </nav>
+		ga("create", "UA-67474823-1", "auto");
+		ga("send", "pageview");
+	      </script>
+	    </head>
+	    <header>
+              <nav class="top-nav orange flow-text">
+                <div class="container">
+                  <div class="nav-wrapper">
+		    <a class="page-title black-text"><strong>A Better Student L Digest</strong></a>
+		  </div>
+                </div>
+              </nav>
+	    </header>
             <ul id="slide-out" class="side-nav fixed">'
               
         html_sidebar = ""
@@ -135,59 +139,62 @@ while true
             html_sidebar += '<li class="waves-effect waves-orange no-padding"><a class="truncate" href="#'+content[:number]+'">'+content[:subject]+'</a></li>'
         }
           
-        html_middle_part = '</ul>
-          <a href="#" data-activates="slide-out" class="button-collapse"><i class="medium material-icons"></i></a>  
+        html_middle_part = 
+	   '</ul>
+            <a href="#" data-activates="slide-out" class="button-collapse"><i class="medium material-icons"></i></a>
           
             <div class="container">
-                <div class="row">
-                    <div class="col s0 l3">&nbsp</div>
-                    <div class="col s12 l6">
+              <div class="row">
+                <div class="col s0 l3">&nbsp</div>
+                  <div class="col s12 l6">
                     <ul id="staggered-test">'
                     
                     
         html_content = ''            
         
         
-        array_of_contents.map.with_index{ |this_message, i|
+        array_of_contents.map.with_index { |this_message, i|
             content = table_of_contents.at(i);
-            html_content += '<li><div id="'+content[:number]+'" class="card hoverable">
-            <div class="card-title black-text orange center-align">
-                <span>'+content[:subject]+'</span>
-            </div>
-                    <div class="card-content">
-                        <p>'+this_message[:message]+'</p>
-                    </div>
-                </div></li>'
+            html_content += 
+	        '<li>
+		   <div id="'+content[:number]+'" class="card hoverable">
+            	     <div class="card-title black-text orange center-align">
+	               <span>'+content[:subject]+'</span>
+            	     </div>
+                     <div class="card-content">
+                       <p>'+this_message[:message]+'</p>
+                     </div>
+                   </div>
+		 </li>'
         }
                     
                     
         
-        html_footer = '</ul></div>
+        html_footer = 
+		   '</ul>
+		  </div>
                 </div>
-                
-            </div>
+              </div>
         
-            <body>
-              <!--Import jQuery before materialize.js-->
-              <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-              <script type="text/javascript" src="js/materialize.min.js"></script>
-              <script type="text/javascript">
+              <body>
+                <!--Import jQuery before materialize.js-->
+                <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+                <script type="text/javascript" src="js/materialize.min.js"></script>
+                <script type="text/javascript">
                   // Initialize collapse button
                   $(".button-collapse").sideNav();
                   $(document).ready(function(){
-                        $("a").click(function(){
-                            // Hide sideNav
-                            $(".button-collapse").sideNav("hide");
-                        });
-                        Materialize.showStaggeredList("#staggered-test")
+                    $("a").click(function(){
+                      // Hide sideNav
+                      $(".button-collapse").sideNav("hide");
                     });
-              </script>
-            </body>
-          </html>'
+                    Materialize.showStaggeredList("#staggered-test")
+                  });
+                </script>
+              </body>
+            </html>'
           
-        
-          
-          html = html_header + html_sidebar + html_middle_part + html_content + html_footer
+            html = html_header + html_sidebar + html_middle_part + html_content + html_footer
           
           File.open("/var/www/html/index.html", 'w') { |file| file.write(html) }
     end
